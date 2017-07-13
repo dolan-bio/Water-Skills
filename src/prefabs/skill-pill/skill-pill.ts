d14f4cc8d5a833daefe48432c2fbdc1188698d7e
Dolan <dolan_miu@hotmail.com>
2017-07-13 23:13:34 +0100

    Removed comments

--- src/prefabs/skill-pill/skill-pill.ts
+++ src/prefabs/skill-pill/skill-pill.ts
@@ -6,27 +6,13 @@ import { BuoyancyManager } from "./buoyancy-manager";
 export class SkillPill extends Phaser.Sprite {
 
     private inWater: boolean;
-    private buoyancyManager: BuoyancyManager;
 
-    constructor(game: Phaser.Game, x: number, y: number, buoyancyManager: BuoyancyManager) {
+    constructor(game: Phaser.Game, x: number, y: number, private buoyancyManager: BuoyancyManager) {
         super(game, x, y);
 
         this.inWater = false;
-        this.buoyancyManager = buoyancyManager;
         this.game.physics.p2.enable(this);
-        // this.water = water;
         this.body.angularVelocity = (Math.random() * 8) - 4;
-        // this.body.debug = true;
-
-        /*var text = this.game.add.text(0, 0, "MyText", { font: '14px Raleway', align: 'center' });
-        text.anchor.setTo(0.5);
-        text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
-        //var textSprite = this.add.sprite(this.world.centerX - 100, this.world.centerY - 200, null);
-        this.addChild(text);
-        //this.physics.enable(textSprite, Phaser.Physics.ARCADE);
-        /*textSprite.body.bounce.y = 1;
-        textSprite.body.gravity.y = 2000;
-        textSprite.body.collideWorldBounds = true;*/
     }
 
     public updatePhysics(point: Phaser.Point, water: Water): void {
@@ -43,15 +29,5 @@ export class SkillPill extends Phaser.Sprite {
         }
 
         this.inWater = this.y > this.game.height / 2 ? true : false;
-        /*var velocity = [];
-        this.body.getVelocityAtPoint(velocity, [0, 0]);
-        var velocityVector = new Phaser.Point(velocity[0], velocity[1]);
-        if (this.position.y > this.water.getWaterLevel(this.position.x).y) {
-            if (!this.splashed) {
-                this.water.splash(this.position.x, velocityVector.getMagnitude() * 3);
-                this.splashed = true;
-            }
-        }
-        this.body.angularVelocity *= 0.99;*/
     }
 }
