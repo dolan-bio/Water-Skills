e99434a4908616dc0eaff77c07cd5597f20c3433
Dolan Miu <dolan_miu@hotmail.com>
2020-10-25 20:35:46 +0000

    Add random colours to text

--- src/prefabs/skill-pill/skill-pill.ts
+++ src/prefabs/skill-pill/skill-pill.ts
@@ -3,13 +3,22 @@ import * as Phaser from "phaser-ce";
 import { Water } from "../water/water";
 import { BuoyancyManager } from "./buoyancy-manager";
 
+const COLOR_PALATES = [
+    ["#D496A7", "#9D695A", "#78E0DC", "#8EEDF7", "#A1CDF1"],
+    ["#E89005", "#EC7505", "#D84A05", "#F42B03", "#E70E02"],
+    ["#BCE784", "#5DD39E", "#348AA7", "#525174", "#513B56"],
+    ["#0DAB76", "#139A43", "#0B5D1E", "#053B06", "#000000"],
+    ["#75DDDD", "#508991", "#172A3A", "#004346", "#09BC8A"],
+];
+
 export class SkillPill extends Phaser.Text {
     private inWater: boolean;
 
     constructor(game: Phaser.Game, x: number, y: number, text: string, private buoyancyManager: BuoyancyManager) {
         super(game, x, y, text, {
+            font: "14px 'Anonymous Pro'",
             fill: "white",
-            backgroundColor: "#000",
+            backgroundColor: COLOR_PALATES[Math.floor(Math.random() * COLOR_PALATES.length)][Math.floor(Math.random() * COLOR_PALATES[0].length)],
         });
 
         this.inWater = false;
